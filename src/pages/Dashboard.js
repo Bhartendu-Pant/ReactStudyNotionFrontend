644b3fbb3cee258c813eed8a52a38d636c4b9b44
Bhartendu <bhartendupant809@gmail.com>
2024-01-11 13:18:42 +0530

    adding study notion frontend

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="flex justify-center items-center h-full text-white text-3xl">Welcome to the Dashboard page</div>
+  )
+}
+
+export default Dashboard
